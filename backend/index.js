3e2177b5df52876d3eaefa82b1d4d99a0472b1a5
headzoo <sean@headzoo.io>
2021-08-30 13:39:39 -0400

    Initializes bodyparser and cors

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
 import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 
 const app = express();
+app.use(cors({ origin: true }));
+app.use(bodyParser.json());
 const port = 3001;
 
 app.get('/', (req, res) => {
