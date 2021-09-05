5b2b42e9377165a45b302045a0b4f34b703d44c5
headzoo <sean@headzoo.io>
2021-09-05 11:06:31 -0400

    Adds the dotenv package and .env-sample

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,16 @@
+import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
 
+dotenv.config();
+
 const app = express();
 app.use(cors({ origin: true }));
-const port = 3001;
 
 app.get('/', (req, res) => {
     res.send('Welcome to the reddit clone!');
 });
 
-app.listen(port, () => {
-    console.log(`Listening on http://localhost:${port}`);
+app.listen(process.env.SERVER_PORT, () => {
+    console.log(`Listening on http://localhost:${process.env.SERVER_PORT}`);
 });
