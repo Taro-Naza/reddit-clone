390022b59d979d4f84007dd5b129cbccd51eca12
headzoo <sean@headzoo.io>
2021-09-05 11:02:20 -0400

    Runs all files through prettier

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,8 @@
 import express from 'express';
 import cors from 'cors';
-import bodyParser from 'body-parser';
 
 const app = express();
 app.use(cors({ origin: true }));
-app.use(bodyParser.json());
 const port = 3001;
 
 app.get('/', (req, res) => {
