856d2a19a75a1e4e136e1ece6d82594b439185d1
headzoo <sean@headzoo.io>
2021-09-06 16:40:11 -0400

    Adds BaseModel class and Users class

--- backend/config.js
+++ backend/config.js
@@ -2,7 +2,14 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const required = ['SERVER_PORT'];
+const required = [
+    'SERVER_PORT',
+    'DB_USER',
+    'DB_PASS',
+    'DB_NAME',
+    'DB_HOST',
+    'DB_PORT'
+];
 required.forEach((key) => {
     if (process.env[key] === undefined) {
         console.error(`Missing configuration value for key ${key}`);
--- backend/models/users.js
+++ backend/models/users.js
@@ -1,7 +1,24 @@
-const getUsers = 'SELECT * FROM "user";';
+import BaseModel from '@models/baseModel';
+import pool from '../../database/database';
+
+/**
+ * Model for the user table.
+ */
+class Users extends BaseModel {
+    /**
+     * Returns the name of the model table
+     */
+    getTableName = () => {
+        return 'user';
+    };
+}
+
+export default new Users(pool);
+
+/* const getUsers = 'SELECT * FROM "user";';
 const getUserByID = 'SELECT * FROM "user" WHERE id = $1;';
 const addUser = `
 INSERT INTO "user" (username, first_name, last_name, birth_date, password, email, avatar_sm, avatar_md, avatar_lg, created_on, updated_on, last_login_ip, is_deleted) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)`;
 const checkEmailExist = 'SELECT *  FROM "user" WHERE email = $1';
 
-module.exports = { getUsers, getUserByID, addUser, checkEmailExist };
+module.exports = { getUsers, getUserByID, addUser, checkEmailExist }; */
