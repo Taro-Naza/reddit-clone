2b0a794f4437d9cf0fd2863cc1bbe31f98c38f8b
headzoo <sean@headzoo.io>
2021-09-12 12:00:36 -0400

    Formats files for latest eslint rules

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -2,17 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Component, Items, Item } from './styles';
 
-const NavBar = () => (
-    <Component>
-        <Items>
-            <Item>
-                <Link to="/">Home</Link>
-            </Item>
-            <Item>
-                <Link to="/login">Login</Link>
-            </Item>
-        </Items>
-    </Component>
-);
+const NavBar = () => {
+    return (
+        <Component>
+            <Items>
+                <Item>
+                    <Link to="/">Home</Link>
+                </Item>
+                <Item>
+                    <Link to="/login">Login</Link>
+                </Item>
+            </Items>
+        </Component>
+    );
+};
 
 export default NavBar;
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import Page from '@components/Page';
 import { Component } from './styles';
 
-const HomePage = () => (
-    <Page title="Reddit Clone">
-        <Component>Welcome!</Component>
-    </Page>
-);
+const HomePage = () => {
+    return (
+        <Page title="Reddit Clone">
+            <Component>Welcome!</Component>
+        </Page>
+    );
+};
 
 export default HomePage;
